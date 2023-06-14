a64fe181034880e3d8030456ff283664b7ed3f57
AngusLu0731 <eric07310416@gmail.com>
2023-06-14 09:01:39 +0800

    add: sendMail auth

--- service/email.ts
+++ service/email.ts
@@ -1,25 +1,38 @@
 import nodemailer from 'nodemailer';
 import config from "../config";
 
-let transporter = nodemailer.createTransport({
-    host: config.MAIL_HOST,
-    port: 587,
-    tls: {
-        // rejectUnauthorized: true,
-        minVersion: "TLSv1.2"
-    },
-    auth: {
-        user: config.MAIL_USER,
-        pass: config.MAIL_PASS,
-    }
-});
-
 export default {
     sendMail: async (
         payload: {
-            to: string, subject: string, text: string, who: string
+            to: string, subject: string, text: string, who: string, user?: string, password?: string
         }
     ) => {
+        let transporter;
+        if (payload.user && payload.password) {
+            transporter = nodemailer.createTransport({
+                host: config.MAIL_HOST,
+                port: 587,
+                tls: {
+                    // rejectUnauthorized: true,
+                    minVersion: "TLSv1.2"
+                },
+                auth: {
+                    user: payload.user,
+                    pass: payload.password,
+                }
+            });
+        } else {
+            transporter = nodemailer.createTransport({
+                host: config.MAIL_HOST,
+                port: 587,
+                tls: {
+                    // rejectUnauthorized: true,
+                    minVersion: "TLSv1.2"
+                },
+            });
+        }
+
+
         let result = null
         await transporter.sendMail({
             from: payload.who,
